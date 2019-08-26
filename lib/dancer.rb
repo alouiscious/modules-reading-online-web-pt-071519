572fd1192b3d7bca54cbd08365fbc045a9173cc3
Renee Sims <info@reneesoul.com>
2019-08-26 01:15:45 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,14 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
+<<<<<<< HEAD
     extend FancyDance::ClassMethods
 
     include FancyDance::InstanceMethods
+=======
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+>>>>>>> 7fa3ad4b97ffe1bc13a19a6b2f9f6654df798454
 
     attr_accessor :name
 
